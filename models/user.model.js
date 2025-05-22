0e442a42009128c65296eec779f7cb5550a78aa0
Md. Shahidul Islam <shanto57575@gmail.com>
2025-05-22 12:11:29 +0600

    signup complete with frontend integration

--- models/user.model.js
+++ models/user.model.js
@@ -10,16 +10,12 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    password: {
-        type: String,
-        required: true
-    },
     role: {
         type: String,
         enum: ["user", "admin"],
         default: "user"
     }
-})
+}, { timestamps: true })
 
 const User = mongoose.model("User", userSchema)
 
